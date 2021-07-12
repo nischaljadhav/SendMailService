25f7fa0711ae633cc54df3da528c7fe8eab0e95c
Nischal.Jadhav <nischal.jadhav@gmail.com>
2021-07-12 15:15:45 +0530

    Nischal -- updated

--- mail_service/src/main/java/com/mailservice/controller/MailController.java
+++ mail_service/src/main/java/com/mailservice/controller/MailController.java
@@ -1,15 +1,21 @@
 package com.mailservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mailservice.services.MailService;
+
 @RestController
 @RequestMapping("/api")
 public class MailController {
+	@Autowired
+	private MailService sendMail;
 	@GetMapping("/sendmail")
 	public String sendMail(){
-		return "success!"; 
+		return sendMail.sendMail();
+		
 	}
 	
 
--- mail_service/src/main/java/com/mailservice/services/MailService.java
+++ mail_service/src/main/java/com/mailservice/services/MailService.java
@@ -1,6 +1,8 @@
 package com.mailservice.services;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,7 @@ import com.microsoft.graph.models.EmailAddress;
 import com.microsoft.graph.models.ItemBody;
 import com.microsoft.graph.models.Message;
 import com.microsoft.graph.models.Recipient;
+import com.microsoft.graph.models.User;
 import com.microsoft.graph.models.UserSendMailParameterSet;
 import com.microsoft.graph.requests.GraphServiceClient;
 
@@ -19,48 +22,64 @@ import com.microsoft.graph.requests.GraphServiceClient;
 public class MailService {
 	
 		public String sendMail(){
+//          Credentials for send mail
+			String CLIENT_ID = "2deef38c-5a6b-4c7b-a46d-534eb8658b0e";
+			String CLIENT_SECRET = "6z3x.PoaSWze6p9PE0SJvIh1l_5-F.d23_";
+			String TENANT_GUID = "78d7cfc1-dedd-4464-b309-74a59265897e";
 			
-//			final ClientSecretCredential clientSecretCredential = new ClientSecretCredentialBuilder()
-//			        .clientId(CLIENT_ID)
-//			        .clientSecret(CLIENT_SECRET)
-//			        .tenantId(TENANT_GUID)
-//			        .build();
-//			
-//			final TokenCredentialAuthProvider tokenCredAuthProvider = new TokenCredentialAuthProvider(SCOPES, clientSecretCredential);
-//			
-//			GraphServiceClient graphClient = GraphServiceClient.builder().authenticationProvider( authProvider ).buildClient();
-//			Message message = new Message();
-//			message.subject = "Meet for lunch?";
-//			ItemBody body = new ItemBody();
-//			body.contentType = BodyType.TEXT;
-//			body.content = "The new cafeteria is open.";
-//			message.body = body;
-//			LinkedList<Recipient> toRecipientsList = new LinkedList<Recipient>();
-//			Recipient toRecipients = new Recipient();
-//			EmailAddress emailAddress = new EmailAddress();
-//			emailAddress.address = "nischal.jadhav@gmail.com";
-//			toRecipients.emailAddress = emailAddress;
-//			toRecipientsList.add(toRecipients);
-//			message.toRecipients = toRecipientsList;
-//			LinkedList<Recipient> ccRecipientsList = new LinkedList<Recipient>();
-//			Recipient ccRecipients = new Recipient();
-//			EmailAddress emailAddress1 = new EmailAddress();
-//			emailAddress1.address = "nischal.cta@gmail.com";
-//			ccRecipients.emailAddress = emailAddress1;
-//			ccRecipientsList.add(ccRecipients);
-//			message.ccRecipients = ccRecipientsList;
-//
-//			boolean saveToSentItems = false;
-//
-//			graphClient.me()
-//				.sendMail(UserSendMailParameterSet
-//					.newBuilder()
-//					.withMessage(message)
-//					.withSaveToSentItems(saveToSentItems)
-//					.build())
-//				.buildRequest()
-//				.post();
-			return null;
+			List<String> SCOPES  = new ArrayList<String>();
+			SCOPES.add("https://graph.microsoft.com/.default");
+//			SCOPES.add("https://outlook.office.com/mail.read/.default");
+//			SCOPES.add("https://outlook.office.com/mail.send/.default");
+			
+			
+			final ClientSecretCredential clientSecretCredential = new ClientSecretCredentialBuilder()
+			        .clientId(CLIENT_ID)
+			        .clientSecret(CLIENT_SECRET)
+			        .tenantId(TENANT_GUID)
+			        .build();
+			
+			final TokenCredentialAuthProvider tokenCredAuthProvider = new TokenCredentialAuthProvider(SCOPES, clientSecretCredential);
+			System.out.println(tokenCredAuthProvider.toString());
+//			GraphServiceClient graphClient = GraphServiceClient.builder().authenticationProvider( tokenCredAuthProvider ).buildClient();
+			
+//			final User me = graphClient.me().buildRequest().get();
+//			System.out.println(me);
+//			return me.displayName;
+			
+			GraphServiceClient graphClient = GraphServiceClient.builder().authenticationProvider( tokenCredAuthProvider ).buildClient();
+			Message message = new Message();
+			message.subject = "Meet for lunch?";
+			ItemBody body = new ItemBody();
+			body.contentType = BodyType.TEXT;
+			body.content = "The new cafeteria is open.";
+			message.body = body;
+			LinkedList<Recipient> toRecipientsList = new LinkedList<Recipient>();
+			Recipient toRecipients = new Recipient();
+			EmailAddress emailAddress = new EmailAddress();
+			emailAddress.address = "nischal.jadhav@gmail.com";
+			toRecipients.emailAddress = emailAddress;
+			toRecipientsList.add(toRecipients);
+			message.toRecipients = toRecipientsList;
+			LinkedList<Recipient> ccRecipientsList = new LinkedList<Recipient>();
+			Recipient ccRecipients = new Recipient();
+			EmailAddress emailAddress1 = new EmailAddress();
+			emailAddress1.address = "nischal.cta@gmail.com";
+			ccRecipients.emailAddress = emailAddress1;
+			ccRecipientsList.add(ccRecipients);
+			message.ccRecipients = ccRecipientsList;
+
+			boolean saveToSentItems = false;
+
+			graphClient.me()
+				.sendMail(UserSendMailParameterSet
+					.newBuilder()
+					.withMessage(message)
+					.withSaveToSentItems(saveToSentItems)
+					.build())
+				.buildRequest()
+				.post();
+			return "success nischal";
 //			
 		}
 
